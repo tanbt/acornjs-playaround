5a10996d983705eaf21a4e7743c76cf963029d09
Tan Bui <tan@vaadin.com>
2019-02-27 11:12:29 +0200

    Get import nodes and template nodes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var acorn = require("acorn");
+var walk = require("acorn-walk");
 
-console.log(acorn.parse(`
+var content = `
 /**
  * Try LitElement https://lit-element.polymer-project.org/guide/try
  * Starting code for 4. Logic
  */
 
 import { LitElement, html } from '@polymer/lit-element';
+import '@polymer/paper-button/paper-button.js';
 
 class MyElement extends LitElement {
   static get properties() {
@@ -21,9 +23,48 @@ class MyElement extends LitElement {
 
   render() {
     return html\`
-      <p>${this.message}</p>
+      <p>\${this.message}</p>
     \`;
   }
 }
 customElements.define('my-element', MyElement);
-`, {sourceType: "module"}));
+`;
+const rootNode = acorn.parse(content, { sourceType: "module" });
+console.log(rootNode);
+
+function getImportDeclarationsNodes(node) {
+    const importDeclarationsNodes = [];
+    walk.simple(rootNode, {
+        ImportDeclaration(node) {
+            importDeclarationsNodes.push(node);
+        }
+    })
+    return importDeclarationsNodes;
+}
+
+function getTemplateNode(root) {
+    let renderFunction = null;
+    walk.simple(root, {
+        MethodDefinition(f) {
+            if (f.key.name == 'render') {
+                renderFunction = f;
+            }
+        }
+    });
+    const htmlTags = [];
+    walk.simple(renderFunction, {
+        TaggedTemplateExpression(node) {
+            htmlTags.push(node);
+        }
+    });
+    return htmlTags[0];
+}
+
+const templateNode = getTemplateNode(rootNode);
+const importNodes = getImportDeclarationsNodes(rootNode);
+
+console.log(importNodes);
+console.log(templateNode);
+
+// const literal = node.source.value;
+//         console.log(literal);
